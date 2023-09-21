50776aaf757df2a531f7aae1898ee0348ce060e8
Ayush Soni <ayusoni86@gmail.com>
2023-09-21 18:25:08 +0530

    day 10 done

---
+++ src/com/java/leetcode75/day10/MoveZeroes_283.java
@@ -0,0 +1,15 @@
+package com.java.leetcode75.day10;
+
+public class MoveZeroes_283 {
+    public void moveZeroes(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+}
